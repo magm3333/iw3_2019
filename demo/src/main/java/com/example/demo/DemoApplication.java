8829fe2700f2828b392ecc3258e12dfad307fad5
magm3333 <magm3333@gmail.com>
2019-09-17 18:46:48 -0300

    Clase 17-9-2019

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -1,14 +1,18 @@
 package com.example.demo;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-import com.example.demo.model.Producto;
-import com.example.demo.persistence.ProductoRepository;
+import com.example.demo.model.Usuario;
+import com.example.demo.persistence.UsuarioRepository;
+import com.example.demo.util.DefaultData;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class DemoApplication implements CommandLineRunner{
 
 	public static void main(String[] args) {
@@ -16,23 +20,25 @@ public class DemoApplication implements CommandLineRunner{
 	}
 
 	
+
 	@Autowired
-	private ProductoRepository productoDAO; 
+	private DefaultData defaultData;
+	
+	@Autowired
+	private UsuarioRepository usuarioDAO;
+	
 	@Override
 	public void run(String... args) throws Exception {
-		Producto p=new Producto();
-		p.setId(1);
-		p.setDescripcion("Arroz");
-		p.setPrecio(45.67);
-		productoDAO.save(p);
 		
-		p=new Producto();
-		p.setId(2);
-		p.setDescripcion("Leche");
-		p.setPrecio(56.33);
-		productoDAO.save(p);
+		defaultData.ensureUserAdmin();
+		
+		List<Usuario> l=usuarioDAO.listadoCustomizado("%a%");
+		System.out.println(l.get(0).getEmail());
 	 
-		
-	}
+		System.out.println(usuarioDAO.cantidadUsuariosHabilitados());
+
+		System.out.println(usuarioDAO.listadoUsuariosDTO().get(0).getUsername());
+		System.out.println(usuarioDAO.listado().get(0).getUsername());
+		}
 
 }
