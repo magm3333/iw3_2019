bfa80384793f39f7f00db1de8c5cbc307d187479
Mariano García Mattío <magm3333@gmail.com>
2019-10-22 16:34:21 -0300

    Clase 22/oct/2019 - Parte 1

--- demo/src/main/resources/static/app.js
+++ demo/src/main/resources/static/app.js
@@ -1,4 +1,9 @@
-angular.module('iw3',[]);
+angular.module('iw3',[ 'ngRoute', 'ngSanitize', 'ngAnimate', 'ngTouch', 'ui.bootstrap',
+	'ngSanitize', 'angularUtils.directives.dirPagination',
+	'angucomplete-alt', 'ngLoadingSpinner', 'ui.select',
+	'ngDragDrop', 'ui-notification',
+	'chart.js', 'ngStomp', 'uiSwitch', 'xeditable', 'ngStorage', 
+	'oitozero.ngSweetAlert', 'dialogs.main' ]);
 
 var app=angular.module('iw3');
 
